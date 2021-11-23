5a49958d6cc3476ad2b77a530d8b72abd5d9b0e7
Paradoru <LicLexa@yandex.ru>
2021-11-23 23:46:22 +0200

    Добавьте файлы проекта.

---
+++ 3.4 Base/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3._4_Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|     x       |      y       |");
+            for (double x = 2; x <= 4; x += 0.2)
+            {
+                double y = Math.Pow(x, 3) + 3 * Math.Pow(x, 2) - 3;
+                Console.WriteLine("|{0,13:F1}|{1,14:F2}|", x, y);
+            }
+        }
+    }
+}
